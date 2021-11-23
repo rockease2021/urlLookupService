487a60e8b1663df7c9abeb60c94420b95964ca17
Rakesh Muthusamy <ec2-user@ip-172-31-27-34.us-east-2.compute.internal>
2021-11-23 07:46:19 +0000

    Stats Improvment

--- src/urllookup.py
+++ src/urllookup.py
@@ -11,16 +11,36 @@ import logging
 urlapp = FastAPI()
 logging.basicConfig(filename='urlapp.log', level=logging.ERROR)
 
-#For debugging
-total_req_count      = 0
-malware_detected     = 0
-malware_not_detected = 0
-total_failed_req     = 0
+class stats(object):
+    total_req_count      = 0
+    malware_detected     = 0
+    malware_not_detected = 0
+    total_failed_req     = 0
+
+    def __init__(self):
+        stats.total_req_count       = 0
+        stats.malware_detected      = 0
+        stats.malware_not_detected  = 0
+        stats.total_failed_req      = 0
+
+    def add_total_req_count(self):
+        stats.total_req_count +=1
+
+    def add_malware_detected(self):
+        stats.malware_detected +=1
+
+    def add_malware_not_detected(self):
+        stats.malware_not_detected +=1
+
+    def add_total_failed_req(self):
+        stats.total_failed_req += 1
 
 redis_ip     = os.getenv('DATABASE') if os.getenv('DATABASE') else "redis"
 redis_port   = os.getenv('REDIS_PORT') if os.getenv('REDIS_PORT') else "6379"
 service_port = os.getenv('APP_PORT') if os.getenv('APP_PORT') else "8090"
 ip_addr      = os.getenv('IP') if os.getenv('IP') else "0.0.0.0"
+
+req_stats = stats()
 
 @urlapp.on_event("startup")
 async def startup():
@@ -40,17 +60,17 @@ def helper():
 @urlapp.get("/stats")
 def urlappStats():
     logging.debug("Get Statistics")
-    logging.info(f"Total reqs :{total_req_count} Total Failed req: {total_failed_req}"
-                 f"Total Malware detected: {malware_detected}, Total Good Req : {malware_not_detected}")
+    logging.info(f"Total reqs :{req_stats.total_req_count} Total Failed req: {req_stats.total_failed_req}"
+                 f"Total Malware detected: {req_stats.malware_detected}, Total Good Req : {req_stats.malware_not_detected}")
 
-    return {"Total Requests": str(total_req_count + total_failed_req),
-            "Malwares_detected" : str(malware_detected),
-            "Malwares_not_detected" : str(malware_not_detected)}
+    return {"Service Name":" ",
+            "Total Requests": str(req_stats.total_req_count + req_stats.total_failed_req),
+            "Malwares_detected" : str(req_stats.malware_detected),
+            "Malwares_not_detected" : str(req_stats.malware_not_detected)}
 
 @urlapp.exception_handler(StarletteHTTPException)
 async def httpExceptionHanlder(Request, exc):
-    global total_failed_req
-    total_failed_req += 1
+    req_stats.add_total_failed_req()
     logging.error("Exception - Invalid URL request")
     return JSONResponse(
         status_code=400,
@@ -77,24 +97,22 @@ async def urlUpdateSingleEntry(type, hostname_and_port, original_path_and_query_
 
 @urlapp.get("/urlinfo/1/{hostname_and_port}/{original_path_and_query_string}")
 async def urlInfoLookup(hostname_and_port, original_path_and_query_string):
-    global total_req_count
-    global malware_detected
-    global malware_not_detected
 
     # Enable for pytest until docker pytest developemnt is done
     # urlapp.client = StrictRedis(host="0.0.0.0", port="6379", db=0)
 
-    total_req_count += 1
+    req_stats.add_total_req_count()
     if not hostname_and_port or not original_path_and_query_string:
         return {"Cause" : "Bad_Request"}
+
     found = 0
     found = await urlapp.client.exists(hostname_and_port + "/" + original_path_and_query_string)
     if found:
         logging.info("Malware found",hostname_and_port + "/" + original_path_and_query_string)
-        malware_detected += 1
+        req_stats.add_malware_detected()
         return {"Cause": "MALWARE_DETECTED"}
     else:
-        malware_not_detected += 1
+        req_stats.add_malware_not_detected()
         return JSONResponse(
             status_code=404,
             content={"Cause": "MALWARE_NOT_DETECTED"},
